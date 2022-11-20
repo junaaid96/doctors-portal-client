d296abb3b9152e5b19014e78d8ddcb6da22f0fa9
Md. Junaidul Islam <info.junaidulislam@gmail.com>
2022-11-20 11:58:19 +0600

    DRY applied

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,73 +1,15 @@
 import React from "react";
+import Banner from "./Banner";
+import Services from "./Services";
 
 const Home = () => {
     return (
         <>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img
-                        src="https://placeimg.com/260/400/arch"
-                        className="max-w-sm rounded-lg shadow-2xl"
-                        alt=""
-                    />
-                    <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat
-                            fugiat ut assumenda excepturi exercitationem quasi.
-                            In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
-                        <button className="btn btn-primary">Get Started</button>
-                    </div>
-                </div>
-            </div>
-
+            <Banner />
             <div className="flex flex-wrap justify-center w-full gap-3 p-12">
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure>
-                        <img
-                            src="https://placeimg.com/400/225/arch"
-                            alt="Shoes"
-                        />
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure>
-                        <img
-                            src="https://placeimg.com/400/225/arch"
-                            alt="Shoes"
-                        />
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure>
-                        <img
-                            src="https://placeimg.com/400/225/arch"
-                            alt="Shoes"
-                        />
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
+                <Services />
+                <Services />
+                <Services />
             </div>
         </>
     );
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -2,9 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+    const menuItems = (
+        <>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/appointment">Appointment</Link>
+            </li>
+            <li>
+                <Link to="/reviews">Reviews</Link>
+            </li>
+            <li>
+                <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+        </>
+    );
+
     return (
         <div className="navbar bg-base-100">
-            <div className="navbar-start">
+            <div className="navbar">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg
@@ -23,51 +46,15 @@ const Navbar = () => {
                         </svg>
                     </label>
                     <ul className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/appointment">Appointment</Link>
-                        </li>
-                        <li>
-                            <Link to="/reviews">Reviews</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact Us</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
+                        {menuItems}
                     </ul>
                 </div>
-                <Link className="btn btn-ghost normal-case text-xl">
-                    daisyUI
+                <Link to="/" className="btn btn-ghost normal-case text-xl">
+                    Doctors Portal
                 </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/appointment">Appointment</Link>
-                    </li>
-                    <li>
-                        <Link to="/reviews">Reviews</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact Us</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                </ul>
+                <ul className="menu menu-horizontal p-0">{menuItems}</ul>
             </div>
         </div>
     );
